b879f5e3e41068aa0d98e483ae79a0a2a8ddfa62
Eugene Zadyra <untrance@gmail.com>
2020-09-28 18:16:10 +0200

    Added temperature sensor

--- thermobox.ino
+++ thermobox.ino
@@ -1,11 +1,19 @@
+#include <DHT.h>
+#include <DHT_U.h>
+#include <LiquidCrystal.h>
 #include <Bounce2.h>
 
 
 #define DEC_BTN_PIN 52
 #define INC_BTN_PIN 53
+#define DHT_PIN 39
+#define DHTTYPE DHT11
 
 Bounce decBtn = Bounce();
 Bounce incBtn = Bounce();
+DHT dht(DHT_PIN, DHTTYPE);
+
+LiquidCrystal lcd(12, 11, 45, 44, 43, 42);
 
 int temp = 20;
 
@@ -14,6 +22,8 @@ void setup() {
   decBtn.interval(25);
   incBtn.attach(INC_BTN_PIN, INPUT_PULLUP);
   incBtn.interval(25);
+  lcd.begin(16, 2);
+  dht.begin();
   Serial.begin(9600);
 }
 
@@ -28,4 +38,13 @@ void loop() {
     temp--;
     Serial.println(temp);
   }
+  lcd.setCursor(2, 0);
+  lcd.print(temp);
+
+  float currentTemp = dht.readTemperature();
+  if (!isnan(currentTemp)) {
+    lcd.setCursor(2, 1);
+    lcd.print(currentTemp);
+  }
+
 }
