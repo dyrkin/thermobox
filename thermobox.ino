fbdcbe3c3533d376b10d622235786bfbf6b73cdf
Eugene Zadyra <untrance@gmail.com>
2020-09-28 22:43:17 +0200

    Added heater switch

--- thermobox.ino
+++ thermobox.ino
@@ -7,6 +7,7 @@
 #define DEC_BTN_PIN 52
 #define INC_BTN_PIN 53
 #define DHT_PIN 39
+#define SWITCH_PIN A0
 #define DHTTYPE DHT11
 
 Bounce decBtn = Bounce();
@@ -15,7 +16,8 @@ DHT dht(DHT_PIN, DHTTYPE);
 
 LiquidCrystal lcd(12, 11, 45, 44, 43, 42);
 
-int temp = 20;
+int keepTemp = 20;
+float sensorTemp;
 
 void setup() {
   decBtn.attach(DEC_BTN_PIN, INPUT_PULLUP);
@@ -23,28 +25,44 @@ void setup() {
   incBtn.attach(INC_BTN_PIN, INPUT_PULLUP);
   incBtn.interval(25);
   lcd.begin(16, 2);
+  lcd.setCursor(2, 0);
+  lcd.print(keepTemp);
   dht.begin();
   Serial.begin(9600);
+  pinMode(SWITCH_PIN, OUTPUT);
 }
 
 void loop() {
   decBtn.update();
   incBtn.update();
+  bool keepTempChanged = false;
+  bool sensorTempChanged = false;
 
   if (incBtn.rose()) {
-    temp++;
-    Serial.println(temp);
+    keepTemp++;
+    keepTempChanged = true;
   } else if (decBtn.rose()) {
-    temp--;
-    Serial.println(temp);
+    keepTemp--;
+    keepTempChanged = true;
   }
-  lcd.setCursor(2, 0);
-  lcd.print(temp);
+
+  if (keepTempChanged) {
+    Serial.println(keepTemp);
+    lcd.setCursor(2, 0);
+    lcd.print(keepTemp);
+  }
 
   float currentTemp = dht.readTemperature();
-  if (!isnan(currentTemp)) {
+  sensorTempChanged = currentTemp != sensorTemp;
+  if (!isnan(currentTemp) && sensorTempChanged) {
+    sensorTemp = currentTemp;
     lcd.setCursor(2, 1);
     lcd.print(currentTemp);
   }
 
+  if (sensorTemp < keepTemp) {
+    digitalWrite(SWITCH_PIN, LOW);
+  } else {
+    digitalWrite(SWITCH_PIN, HIGH);
+  }
 }
